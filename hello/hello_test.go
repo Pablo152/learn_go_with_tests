a54aae64fb34c5af2f4cd3a728af20550aa54c21
Pablo Azuaje <pablo45154@gmail.com>
2021-09-08 14:36:51 -0500

    Refactored tests and added functionality on Hello function

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -4,15 +4,30 @@ import (
 	"testing"
 )
 
-func error(t *testing.T, got string, want string) {
-	t.Errorf("got %q want %q", got, want)
-}
-
 func TestHello(t *testing.T) {
-	got := Hello("Pablo")
-	want := "Hello, Pablo"
 
-	if got != want {
-		error(t, got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Pablo")
+		want := "Hello, Pablo"
+
+		if got != want {
+			assertCorrectMessage(t, got, want)
+		}
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			assertCorrectMessage(t, got, want)
+		}
+	})
 }
